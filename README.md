61351644d49f3dfb33492c00708e11944c0e785f
Adam Nagy <nagy.adamn@gmail.com>
2022-06-01 14:56:06 +0200

    Implement console game
    Add checkGame function. Initialize computer player. Add buttons for UI

--- README.md
+++ README.md
@@ -19,11 +19,26 @@ determine the winner based on the two hands
 A player has a hand and a score (name? default: P1)
 - what's the name of the computer?
 
+3 possibilites for you, based on your picks
+1. you have _rock_
+2. you have _paper_
+3. you have _scissors_
+
+
 ## a round
 player picks hand (1 - rock, 2 - paper, 3 - scissors)
 1. 
 display the hands - eg. P1: rock Computer: rock
 determine the outcome - "draw - play again", "rock beats scissors, P1 gets a point"
+1. if the hands are the same -> start the round again | THIS ELIMINATES EQUAL HANDS, RETURN
+1. if the hands are _rock_ -> 
+    if other hand is _scissors_: +1 to you
+    else: +1 to other (other is _paper_)
+2. if hand is _paper_:
+    if other is _rock_ -> + 1
+    else: +1 to other (_scissors_)
+3. if handOne is _scissors_ -> 
+    if other hand is 
 calculate who gets the point
 
 # rules
--- js/script.js
+++ js/script.js
@@ -1,26 +1,69 @@
 class Player {
-    constructor(name){
-        this.name = name;
-        this.hand = null;
-        this.score = 0;
-    }
-    displayScore() {
-        console.log(this.score);
-    }
-    set hand(hand) {
-        this.hand = hand;
-    }
-    get hand() {
-        return this.hand
-    }
-    clearHand() {
-        this.hand = null;
-    }
-    addScore() {
-        this.score++;
-    }
-    get score() {
-        return this.score
-    }
+  constructor(name = "P1") {
+    this.name = name;
+    this.hand = null;
+    this.score = 0;
+  }
+  displayScore() {
+    console.log(this.score);
+  }
+  set hand(h) {
+    this._hand = h;
+  }
+  get hand() {
+    return this._hand;
+  }
+  get score() {
+    return this._score;
+  }
+  set score(s) {
+    this._score = s;
+  }
+  clearHand() {
+    this.hand = null;
+  }
+  addScore() {
+    this.score++;
+  }
 }
 
+class Game {
+  constructor(rounds) {
+    this.rounds = rounds;
+  }
+}
+
+const randomHand = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randomPick = Math.floor(Math.random() * 3);
+  return options[randomPick];
+};
+
+const checkWinner = (P1, P2) => {
+  if (P1.hand === P2.hand) return;
+  let score = null;
+  if (P1.hand === "rock" && P2.hand === "scissors") {
+      P1.addScore();
+  }
+  if (P1.hand === "paper" && P2.hand === "rock") {
+      P1.addScore();
+  }
+  if (P1.hand === "scissors" && P2.hand === "paper") {
+      P1.addScore();
+  }
+};
+
+const options = document.querySelector(".options");
+
+options.addEventListener("click", (e) => {
+   p1.hand = e.target.innerHTML.toLowerCase();
+   computer.hand = randomHand();
+   checkWinner(p1, computer);
+   checkWinner(computer, p1);
+   console.log(`${p1.name}: ${p1.score}`, `${computer.name}: ${computer.score}`);
+});
+
+const computer = new Player('computer');
+const p1 = new Player();
+
+// console.log(randomHand());
