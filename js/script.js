7b63fc056077c10ed3794c0afae4daebddeacbbc
Adam Nagy <nagy.adamn@gmail.com>
2022-06-02 11:15:39 +0200

    Style main page

--- js/script.js
+++ js/script.js
@@ -30,17 +30,17 @@ class Player {
 }
 
 class HumanPlayer extends Player {
-  constructor(name) {
+  constructor(name = 'Rando') {
     super(name);
     this._addControls();
     this._changeName();
   }
   _addControls() {
-    document.querySelector(".controls").style.display = "block";
+    document.querySelector(".controls").style.display = "flex";
   }
   _changeName() {
     const nameHeading = document.querySelector(".name");
-    nameHeading.innerHTML = this.name;
+    nameHeading.innerHTML = this.name || "Rando";
   }
 }
 
